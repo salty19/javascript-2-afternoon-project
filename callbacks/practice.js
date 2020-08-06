c604b05731e472063695be358529793d6de3e009
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-06 16:23:03 -0700

    cannot do

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -28,7 +28,11 @@
 */
 
 // Code Here 
+function first(array){
+  console.log(array)
+}
 
+function 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
